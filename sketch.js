69b79824cb3fa09b9788edab9f69e89a5af394d9
KingCoder-pixel <77515676+KingCoder-pixel@users.noreply.github.com>
2021-02-07 15:58:51 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var movingRect,fixedRect;
+var car, wall;
+
+function setup()
+{
+
+ createCanvas(1200,800);
+  fixedRect = createSprite(600,400,50,80);
+  fixedRect.shapeColor = "blue";
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "blue";
+  car = createSprite(200, 200, 30, 30);
+  car.shapeColor = "purple";
+  car.velocityX = 4;
+  wall = createSprite(1000, 200, 30, 80);
+  wall.shapeColor = "yellow";
+
+
+}
+
+function draw()
+{
+
+background(0);
+
+console.log(movingRect.x - fixedRect.x);
+movingRect.x = World.mouseX;
+movingRect.y = World.mouseY;
+
+if(isTouching(movingRect, wall))
+{
+  textSize(30);
+  text("as it is touching", 300, 400);
+}
+
+collide(car,wall);
+
+
+
+
+drawSprites();
+
+}
+
+
+
